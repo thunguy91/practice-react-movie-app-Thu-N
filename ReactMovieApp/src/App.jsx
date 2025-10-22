c02ea28bf2a23ade82f1df9ddebefba48d004fc4
Thu Nguyen <thunguy91@gmail.com>
2025-10-22 11:53:06 -0400

    MovieSelector.jsx created

---
+++ ReactMovieApp/src/App.jsx
@@ -0,0 +1,14 @@
+
+import './App.css'
+import MovieSelector from './component/MovieSelector'
+
+function App() {
+
+  return (
+    <div>
+      <MovieSelector/>
+    </div>
+  )
+}
+
+export default App
